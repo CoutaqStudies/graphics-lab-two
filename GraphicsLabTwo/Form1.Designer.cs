043993c21aa152ba1663353ced88f6dd15d120ee
Michael Melikov <coutaq@gmail.com>
2020-10-19 11:14:38 +0300

    Initial commit

---
+++ GraphicsLabTwo/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿using System.Windows.Forms;
+
+namespace GraphicsLabTwo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonInches = new System.Windows.Forms.Button();
+            this.buttonMm = new System.Windows.Forms.Button();
+            this.buttonPixels = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.White;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 500F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1000, 601);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 4;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.Controls.Add(this.buttonClear, 3, 0);
+            this.tableLayoutPanel2.Controls.Add(this.buttonInches, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.buttonMm, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.buttonPixels, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 503);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 95F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(994, 95);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.buttonClear.BackColor = System.Drawing.Color.MintCream;
+            this.buttonClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonClear.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.buttonClear.ForeColor = System.Drawing.Color.FromArgb(((int) (((byte) (64)))), ((int) (((byte) (64)))), ((int) (((byte) (64)))));
+            this.buttonClear.Location = new System.Drawing.Point(809, 31);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(120, 32);
+            this.buttonClear.TabIndex = 3;
+            this.buttonClear.Text = "Очистить";
+            this.buttonClear.UseVisualStyleBackColor = false;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // buttonInches
+            // 
+            this.buttonInches.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.buttonInches.BackColor = System.Drawing.Color.MintCream;
+            this.buttonInches.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonInches.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.buttonInches.ForeColor = System.Drawing.Color.FromArgb(((int) (((byte) (64)))), ((int) (((byte) (64)))), ((int) (((byte) (64)))));
+            this.buttonInches.Location = new System.Drawing.Point(560, 31);
+            this.buttonInches.Name = "buttonInches";
+            this.buttonInches.Size = new System.Drawing.Size(120, 32);
+            this.buttonInches.TabIndex = 2;
+            this.buttonInches.Text = "Inches";
+            this.buttonInches.UseVisualStyleBackColor = false;
+            this.buttonInches.Click += new System.EventHandler(this.buttonInches_Click);
+            // 
+            // buttonMm
+            // 
+            this.buttonMm.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.buttonMm.BackColor = System.Drawing.Color.MintCream;
+            this.buttonMm.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonMm.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.buttonMm.ForeColor = System.Drawing.Color.FromArgb(((int) (((byte) (64)))), ((int) (((byte) (64)))), ((int) (((byte) (64)))));
+            this.buttonMm.Location = new System.Drawing.Point(312, 31);
+            this.buttonMm.Name = "buttonMm";
+            this.buttonMm.Size = new System.Drawing.Size(120, 32);
+            this.buttonMm.TabIndex = 1;
+            this.buttonMm.Text = "Millimeters";
+            this.buttonMm.UseVisualStyleBackColor = false;
+            this.buttonMm.Click += new System.EventHandler(this.buttonMm_Click);
+            // 
+            // buttonPixels
+            // 
+            this.buttonPixels.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.buttonPixels.BackColor = System.Drawing.Color.MintCream;
+            this.buttonPixels.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonPixels.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.buttonPixels.ForeColor = System.Drawing.Color.FromArgb(((int) (((byte) (64)))), ((int) (((byte) (64)))), ((int) (((byte) (64)))));
+            this.buttonPixels.Location = new System.Drawing.Point(64, 31);
+            this.buttonPixels.Name = "buttonPixels";
+            this.buttonPixels.Size = new System.Drawing.Size(120, 32);
+            this.buttonPixels.TabIndex = 0;
+            this.buttonPixels.Text = "Pixels";
+            this.buttonPixels.UseVisualStyleBackColor = false;
+            this.buttonPixels.Click += new System.EventHandler(this.buttonPixels_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (240)))), ((int) (((byte) (248)))), ((int) (((byte) (255)))));
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1000, 500);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1000, 601);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MinimumSize = new System.Drawing.Size(1016, 640);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonInches;
+        private System.Windows.Forms.Button buttonMm;
+        private System.Windows.Forms.Button buttonPixels;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+
+        #endregion
+    }
+}
---
+++ GraphicsLabTwo/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//Сделал Меликов Михаил ИВТ-1 8 вариант
+namespace GraphicsLabTwo
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        public Form1()
+        {
+            InitializeComponent();
+            g = pictureBox1.CreateGraphics();
+        }
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+        }
+
+        private void DrawLines(float penSize, float width, float height)
+        {
+            var pen = new Pen(Color.Crimson, penSize);
+            g.DrawRectangle(pen, 0, 0, width, height);
+            g.DrawLine(pen, 0, height / 2, width, height / 2);
+            g.DrawLine(pen, width / 2, height, width / 2, -height);
+        }
+        private void DrawFunction(float penSize)
+        {
+            float widthMultiplier, heightMultiplier;
+            var width = pictureBox1.Width-1;
+            var height = pictureBox1.Height-1;
+            Clear();
+            switch (g.PageUnit)
+            {
+                case GraphicsUnit.Millimeter:
+                    widthMultiplier = g.DpiX / 25.4f;
+                    heightMultiplier = g.DpiY / 25.4f;
+                    break;
+                case GraphicsUnit.Inch:
+                    widthMultiplier = g.DpiX;
+                    heightMultiplier = g.DpiY;
+                    break;
+                default:
+                    widthMultiplier = 1f;
+                    heightMultiplier = 1f;
+                    break;
+            }
+            DrawLines(penSize, (width-1)/widthMultiplier, (height-1)/heightMultiplier);
+            var pen = new Pen(Color.LimeGreen, penSize);
+
+            int dex = 0, dey = 0;
+            double x = -Math.PI * 2;
+            for (int ex = 0; ex <= width/widthMultiplier; ex++)
+            {
+                int y = (int)(-function(x)*200+250);
+                int ey = (int)(y/heightMultiplier);
+                g.DrawLine(pen, dex, dey, ex, ey);
+                dex = ex; dey = ey;
+                x = x + (Math.PI * 4) / (width/widthMultiplier);
+            }
+        }
+        private double function(double x)
+        {
+            return (4 * Math.Pow(x, 2) + 3 * x);
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        private void Clear()
+        {
+            g.Clear(Color.FromArgb(240,248,255));
+        }
+
+        private void buttonPixels_Click(object sender, EventArgs e)
+        {
+            g.PageUnit = GraphicsUnit.Pixel;
+            DrawFunction(1f);
+        }
+
+        private void buttonMm_Click(object sender, EventArgs e)
+        {
+            g.PageUnit = GraphicsUnit.Millimeter;
+            DrawFunction(0.1f);
+        }
+
+        private void buttonInches_Click(object sender, EventArgs e)
+        {
+            g.PageUnit = GraphicsUnit.Inch;
+            DrawFunction(0.005f);
+        }
+    }
+}
